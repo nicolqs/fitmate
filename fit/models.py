f253e8c74cf82e290602a0256e4a2e6a70c712a1
Nico <nicolqs@gmail.com>
2016-03-19 13:21:29 -0700

    Simplify models, get manyToMany dishes - restaurant, update view, dump JSON

--- fit/models.py
+++ fit/models.py
@@ -5,12 +5,12 @@ from django.utils.encoding import python_2_unicode_compatible
 @python_2_unicode_compatible  # only if you need to support Python 2
 class Restaurant(models.Model):
     name = models.CharField(max_length=50)
-    description = models.CharField(max_length=255)
-    lat = models.CharField(max_length=20)
-    lng = models.CharField(max_length=20)
+    description = models.CharField(max_length=255, blank=True)
+    lat = models.CharField(max_length=20, blank=True)
+    lng = models.CharField(max_length=20, blank=True)
     address = models.CharField(max_length=100)
     zipcode = models.CharField(max_length=5)
-    category = models.CharField(max_length=50)
+    category = models.CharField(max_length=50, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,11 +21,15 @@ class Diet(models.Model):
     def __str__(self):
         return self.name
 
+def get_restaurant():
+    return Restaurant.objects.get(id=1)
+
 class Dish(models.Model):
+    restaurant = models.ManyToManyField(Restaurant)
     name = models.CharField(max_length=50)
-    diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
-    image = models.CharField(max_length=300)
-    description = models.CharField(max_length=255)
+    image = models.CharField(max_length=300, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    diet = models.ManyToManyField(Diet)
 
     def __str__(self):
         return self.name
@@ -34,8 +38,5 @@ class User(models.Model):
     login = models.CharField(max_length=50)
     favourite_diet = models.ForeignKey(Diet, on_delete=models.CASCADE)
 
-
     def __str__(self):
         return self.login
-
-
--- fit/views.py
+++ fit/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
+from django.core import serializers
+from .models import Dish
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    dish_json = serializers.serialize('json', Dish.objects.all())
+    return  HttpResponse( dish_json, content_type="application/json")
+    #JsonResponse({ "dishes": dish_json})
